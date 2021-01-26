c92044f80280988a26700e185dec137d86d66ad7
NetologyQA12 <dvb90@bk.ru>
2021-01-26 12:58:16 +0700

    fix

--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -3,6 +3,7 @@ package ru.netology.repository;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductRepositoryTest {
